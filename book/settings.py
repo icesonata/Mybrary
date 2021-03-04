e0724c93d3c1718d701e9148cd1b00824f9f8f2c
Long Nguyen <hoanglong1622000@gmail.com>
2021-03-04 23:16:24 +0700

    delete key

--- book/settings.py
+++ book/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kty-xgv27b9)ilyk8$n@e)n4wdjj=$i45hsk$!!+=cs*3wr_hj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
